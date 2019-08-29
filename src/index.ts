a72f441a4e073efc4ae2978cd34d42bef8eff7e0
subal <subal@pixiv.com>
2019-08-29 22:08:13 +0900

    無理やり動かすことに成功した

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import aTable from 'a-table'
 import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk'
 
+/**
+ * HACK: sandbox iframe なので window.alert すると意図しない挙動になる
+ */
+window.alert = console.log
+window.confirm = (message: string) => {
+  console.log(message)
+  return true
+}
+
+const config = {
+  lang: 'ja',
+  selector: {
+    option: [{ label: '.hidden クラスをつける', value: 'hidden' }]
+  }
+}
+
 class Table {
-  private table: aTable
+  private table = new aTable(this.el, config)
 
   constructor(private readonly el: HTMLElement, private readonly sdk: FieldExtensionSDK) {
-    this.table = new aTable(el, {})
     this.table.afterEntered = this.onChange.bind(this)
     this.table.afterRendered = this.onChange.bind(this)
-
     this.onChange()
-    this.subscribe()
   }
 
   private onChange() {
@@ -20,18 +33,6 @@ class Table {
 
     this.sdk.field.setValue(this.table.getTable())
   }
-
-  private subscribe() {
-    this.sdk.field.onValueChanged((next: any) => {
-      if (!next) {
-        return
-      }
-
-      if (confirm('Table is changed by external event. reload ?')) {
-        location.reload()
-      }
-    })
-  }
 }
 
 init(_sdk => {
@@ -39,7 +40,34 @@ init(_sdk => {
   sdk.window.startAutoResizer()
 
   const el = document.getElementById('root')
-  if (el) {
-    new Table(el, sdk)
+  if (!el) {
+    return
   }
+
+  const html = parseInitialValue(sdk)
+  if (html) {
+    el.innerHTML = html
+  }
+
+  new Table(el, sdk)
 })
+
+const defaultTemplate = `
+<tr>
+  <th>head1</th>
+  <th>head2</th>
+</tr>
+<tr>
+  <td>body1</td>
+  <td>body2</td>
+</tr>
+`
+
+const parseInitialValue = (sdk: FieldExtensionSDK) => {
+  const raw = sdk.field.getValue()
+  const domparser = new DOMParser()
+  const html = domparser.parseFromString(raw, 'text/html')
+  const table = html.querySelector('table')
+
+  return table ? table.innerHTML : defaultTemplate
+}
